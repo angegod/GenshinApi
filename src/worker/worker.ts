026cb3c8ba0826c87663420fd73f40b2c99c08f7
Ange <a0909219938@gmail.com>
2026-03-15 23:28:11 +0800

    修正聖遺物分數比對標準

--- src/worker/worker.ts
+++ src/worker/worker.ts
@@ -69,8 +69,9 @@ onmessage = function (event) {
     //將沒有被鎖住不可計算的詞條倒裝
     let MainData=charStandard[MainAffix.type];
     let result:number[] = [];
-    let origin=relicScore(partsIndex,charStandard,SubData,MainData);
+    
     //先算原本的遺器的分數
+    let origin=relicScore(partsIndex,charStandard,SubData,MainData);
 
     let p1=new Promise(async (resolve,reject)=>{
         combination.forEach((c,index)=>{
@@ -141,7 +142,12 @@ onmessage = function (event) {
             {rank:'C',stand:15,color:'rgb(163, 230, 53)',tag:'C'},
             {rank:'D',stand:0 ,color:'rgb(22,163,74)',tag:'D'}
         ];
-        let overScoreList=JSON.parse(JSON.stringify(result)).filter((num:number)=>num-deviation>Number(origin));
+        console.log('計算結果arr:',result);
+        console.log('容忍誤差:',deviation);
+        console.log('聖遺物初始分數:',origin);
+        let overScoreList = result
+            .map(num => Math.floor(num * 10) / 10)
+            .filter((num:number) => num - deviation > Number(origin));
         let expRate = Number((overScoreList.length / result.length));
         let copy=JSON.parse(JSON.stringify(result));
         let relicrank:RelicScoreStand|undefined=undefined;
